7166dd7d9e6a7e7b4f7acd284b8d1d46a2222261
Anjan Prasad <97271459+ItzzInfinity@users.noreply.github.com>
2024-09-01 11:30:37 +0530

    Update and rename test_I2C_Radio.ino to Src/test_I2C_Radio.ino

--- Src/test_I2C_Radio.ino
+++ Src/test_I2C_Radio.ino
@@ -1,37 +1,3 @@
-///
-/// \file  TestRDA5807M.ino
-/// \brief An Arduino sketch to operate a SI4705 chip based radio using the Radio library.
-///
-/// \author Matthias Hertel, http://www.mathertel.de
-/// \copyright Copyright (c) 2014 by Matthias Hertel.\n
-/// This work is licensed under a BSD style license. See http://www.mathertel.de/License.aspx
-///
-/// \details
-/// This sketch implements a "as simple as possible" radio without any possibility to modify the settings after initializing the chip.\n
-/// The radio chip is initialized and setup to a fixed band and frequency. These settings can be changed by modifying the
-/// FIX_BAND and FIX_STATION definitions.
-///
-/// Open the Serial console with 57600 baud to see the current radio information.
-///
-/// Wiring
-/// ------
-/// The RDA5807M board/chip has to be connected by using the following connections:
-///
-/// | Signal       | Arduino UNO | ESP8266 | ESP32  | Radio chip signal |
-/// | ------------ | ------------| ------- | ------ | ----------------- |
-/// | VCC (red)    | 3.3V        | 3v3     | 3v3    | VCC               |
-/// | GND (black)  | GND         | GND     | GND    | GND               |
-/// | SCL (yellow) | A5 / SCL    | D1      | 22     | SCLK              |
-/// | SDA (blue)   | A4 / SDA    | D2      | 21     | SDIO              |
-///
-/// The locations of the signals on the RDA5807M side depend on the board you use.
-/// More documentation is available at http://www.mathertel.de/Arduino
-/// Source Code is available on https://github.com/mathertel/Radio
-///
-/// ChangeLog:
-/// ----------
-/// * 05.12.2014 created.
-/// * 19.05.2015 extended.
 
 #include <Arduino.h>
 #include <Wire.h>
@@ -39,10 +5,9 @@
 #include <radio.h>
 #include <RDA5807M.h>
 
-// ----- Fixed settings here. -----
-#define FIX_BAND RADIO_BAND_FM                                                                                     ///< The band that will be tuned by this sketch is FM.
-#define FIX_STATION 10400                                                                                          ///< The station that will be tuned by this sketch is 89.30 MHz.
-#define FIX_VOLUME 13                                                                                              ///< The volume that will be set by this sketch is level 4.
+#define FIX_BAND RADIO_BAND_FM                                                                                     /// The band that will be tuned by this sketch is FM.
+#define FIX_STATION 10400                                                                                          /// The station that will be tuned by this sketch is 104.0 MHz.
+#define FIX_VOLUME 13                                                                                              /// The volume that will be set by this sketch is level 13 .
 const int buttonPin = 3;                                                                                           /// GPIO pin connected to the input button
 RDA5807M radio;                                                                                                    /// Create an instance of Class for RDA5807M Chip
 int count = 0;                                                                                                     /// Counter variable
@@ -69,10 +34,6 @@ void setup() {
                                                                                                                    /// Enable information to the Serial port
   radio.debugEnable(true);
   radio._wireDebug(false);
-
-                                                                                                                   /// Set FM Options for Europe
-  //radio.setup(RADIO_FMSPACING, RADIO_FMSPACING_100);   // for EUROPE
-  //radio.setup(RADIO_DEEMPHASIS, RADIO_DEEMPHASIS_50);  // for EUROPE
 
                                                                                                                    /// Initialize the Radio
   if (!radio.initWire(Wire)) {
